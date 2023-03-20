3b8973895fff34ff121b2ddf057c4ab4d999ba56
unknown <gaidenkoivan1@gmail.com>
2023-03-20 17:18:08 +0300

    stable version

--- claimer.js
+++ claimer.js
@@ -2,10 +2,10 @@ import { ethers } from "ethers";
 import { erc20_abi, claim_abi } from "./abis.js";
 import { Alchemy, Network } from "alchemy-sdk";
 import chalk from "chalk";
-let pasta = `done by @Zetsubouq`
+let pasta = `done by @Zetsubouq 0x5c29C60e14643c6f04Bcf767170b4e785CB5F4D3`
 const settings = {
   apiKey: "<--alchemy_wss_key-->", // Replace with your Alchemy API KEY. 
-                                              // (not https://... or ws://, just key)
+                                   // (not https://... or ws://, just key)
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 const alchemy = new Alchemy(settings);
@@ -24,8 +24,8 @@ let destination_address_array = [
 let rpc_array = [
     // any rpc you can find... (https://...)
     // can be even 1, but the more the better
-    "<--alchemy_https_key-->",
-    "<--another_alchemy_https_key-->",
+    "https://...",
+    "another_https://...",
 ]
 
 
@@ -59,9 +59,14 @@ async function sendClaimAndTransfer(rpc, prv_key, current_nonce, to_addr, claima
         // MAX GAS USED TO CLAIM = 0.005 eth ~= 9$
         nonce: current_nonce,
     })
-    .then(async a => {
-        try {
-        let transferTx = token.connect(wallet).transferFrom(wallet.address, to_addr, "0", {
+    } catch(error) {
+        console.log(chalk.red("error on claim occured"));
+        console.log(chalk.red("wallet: ", wallet.address));
+        console.log(error);
+    }
+    await new Promise(r => setTimeout(r, 30));
+    try {
+        let transferTx = token.connect(wallet).transferFrom(wallet.address, to_addr, claimableAmount, {
             gasLimit: "0x4C4B40",// 5kk in case gas on L1 is expensive.. Read about arbitrums 2D fees to learn more
             gasPrice: "0x3B9ACA00", // 1kkk = 1 gwei in case network is  overloaded
         // MAX GAS USED TO CLAIM = 0.005 eth ~= 9$
@@ -71,14 +76,8 @@ async function sendClaimAndTransfer(rpc, prv_key, current_nonce, to_addr, claima
         console.log(chalk.red("error on transfer occured.."));
         console.log(chalk.red("wallet: ", wallet.address));
         console.log(error);
-        return "";
     }
-    })
-    } catch(error) {
-        console.log(chalk.red("error occured"));
-        console.log(chalk.red("wallet: ", wallet.address));
-        console.log(error);
-    }
+    
     
 }
 
@@ -99,8 +98,8 @@ await prepare();
 alchemy.ws.on("block", 
 async (blockNumber) => {
 console.log(blockNumber);
-if (blockNumber == 16890400) { // 16890400
-    await new Promise(r => setTimeout(r, 1000));
+if (blockNumber >= 16890400) { // 16890400
+    await new Promise(r => setTimeout(r, 700));
     sendMeMoneyBitch();
     alchemy.ws.removeAllListeners();
 }
